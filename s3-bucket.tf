81235d5513f298668c57982cc0679caf8a64aaf2
Basitha Gamage <39352100+bgaj203@users.noreply.github.com>
2023-03-20 11:38:38 +1100

    Fixed issues with s3 version

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3_bucket" {
   source  = "app.terraform.io/basitha-com/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket = "my-s3-bucket"
+  bucket = "basitha-gamage-jhbsadgljsadkjlbsad"
   acl    = "private"
 
   versioning = {
